70cc841447dffe2841f7aec7db21a83f781476d9
Evgeniy <feshchenko.evgeniy03@gmail.com>
2023-04-24 22:34:29 +0300

    update

--- src/components/CardList/CardList.jsx
+++ src/components/CardList/CardList.jsx
@@ -1,12 +1,29 @@
 import { CardListItem } from "../CardListItem/CardListItem";
-
+import PropTypes from "prop-types";
 export const CardList = ({ data }) => {
-  console.log(data);
   return (
     <ul>
-      {data.map((data) => (
-        <CardListItem key={data.id} data={data} />
+      {data.map(({ id, user, tweets, followers, avatar }) => (
+        <CardListItem
+          key={id}
+          user={user}
+          tweets={tweets}
+          followers={followers}
+          avatar={avatar}
+        />
       ))}
     </ul>
   );
+};
+
+CardList.propTypes = {
+  data: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.string.isRequired,
+      user: PropTypes.string.isRequired,
+      tweets: PropTypes.number.isRequired,
+      followers: PropTypes.number.isRequired,
+      avatar: PropTypes.string.isRequired,
+    })
+  ),
 };
--- src/components/CardListItem/CardListItem.jsx
+++ src/components/CardListItem/CardListItem.jsx
@@ -1,11 +1,37 @@
-export const CardListItem = ({ data }) => {
-  console.log(data);
+import PropTypes from "prop-types";
+import { useState } from "react";
+export const CardListItem = ({ user, tweets, followers, avatar }) => {
+  const [following, setFollowing] = useState(followers);
+  const [boolean, setBoolean] = useState(false);
+  const [text, setText] = useState("Follow");
+  const handleClick = () => {
+    if (!boolean) {
+      setBoolean(true);
+      setText("Following");
+      setFollowing((following) => following + 1);
+    } else {
+      setBoolean(false);
+      setFollowing((following) => following - 1);
+      setText("Follow");
+    }
+  };
+
   return (
     <li>
-      <p>{data.user}</p>
-      <p>{data.followers}</p>
-      <p>{data.tweets}</p>
-      <img src={data.avatar} alt="avatar" />
+      <img src={avatar} alt="avatar" />
+      <p>{user}</p>
+      <p>{tweets}</p>
+      <p>{following}</p>
+      <button type="text" onClick={handleClick}>
+        {text}
+      </button>
     </li>
   );
+};
+
+CardListItem.propTypes = {
+  user: PropTypes.string.isRequired,
+  tweets: PropTypes.number.isRequired,
+  followers: PropTypes.number.isRequired,
+  avatar: PropTypes.string.isRequired,
 };
